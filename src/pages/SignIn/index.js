2bd194ebf066bfd81bc9870148ea8a5b539f6da6
Bruno Girome <brunogirome@gmail.com>
2020-05-02 20:44:56 -0300

    Login and Register styles created

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <img src={logo} alt="gobarber-logo" />
+      <form>
+        <input type="email" placeholder="Your e-mail" />
+        <input type="password" placeholder="Your password" />
+
+        <button type="submit">Access</button>
+
+        <Link to="/register">Create a free account</Link>
+      </form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="gobarber-logo" />
+      <form>
+        <input type="text" placeholder="Complete name" />
+        <input type="email" placeholder="Your e-mail" />
+        <input type="password" placeholder="Your password" />
+
+        <button type="submit">Create account</button>
+
+        <Link to="/">I'm already registered</Link>
+      </form>
+    </>
+  );
 }
