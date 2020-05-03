8dd0f7d1683e79dd003977a3fe915935f1d31dc0
Bruno Girome <brunogirome@gmail.com>
2020-05-03 00:14:39 -0300

    Signin and Signup pages

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    alert('hello');
+  }
+
   return (
     <>
       <img src={logo} alt="gobarber-logo" />
-      <form>
-        <input type="email" placeholder="Your e-mail" />
-        <input type="password" placeholder="Your password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Your e-mail" />
+        <Input name="password" type="password" placeholder="Your password" />
 
         <button type="submit">Access</button>
 
         <Link to="/register">Create a free account</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    alert('hello');
+  }
+
   return (
     <>
       <img src={logo} alt="gobarber-logo" />
-      <form>
-        <input type="text" placeholder="Complete name" />
-        <input type="email" placeholder="Your e-mail" />
-        <input type="password" placeholder="Your password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Complete name" />
+        <Input name="email" type="email" placeholder="Your e-mail" />
+        <Input name="password" type="password" placeholder="Your password" />
 
         <button type="submit">Create account</button>
 
-        <Link to="/">I'm already registered</Link>
-      </form>
+        <Link to="/">Already registered</Link>
+      </Form>
     </>
   );
 }
