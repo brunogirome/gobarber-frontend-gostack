efabfa654c2eaec2cd8fcf22eb2e2b9364786cbb
Bruno Girome <brunogirome@gmail.com>
2020-05-04 21:40:23 -0300

    Created the Profile Update form

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
-export default function Profile() {
-  return <div />;
+function Profile() {
+  const profile = useSelector((state) => state.user.profile);
+
+  async function handleSubmit(data) {}
+
+  return (
+    <Container>
+      {/* Rocketseat unform has the 'initialData' property to set variables
+      on input fields based on element 'name' property */}
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Complete name" />
+        <Input name="email" type="email" placeholder="Email name" />
+
+        <hr />
+
+        <Input name="oldPassword" type="password" placeholder="Your password" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Your new password"
+        />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirm your new password"
+        />
+
+        <button type="submit">Update profile</button>
+      </Form>
+
+      <button type="button">Exit GoBarber</button>
+    </Container>
+  );
 }
+
+export default Profile;
---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,84 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  max-width: 600px;
+  margin: 50px auto;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    input {
+      background: rgba(0, 0, 0, 0.1);
+      border: 0;
+      border-radius: 4px;
+      height: 44px;
+      padding: 0 15px;
+      color: #fff;
+      margin: 0 0 10px;
+
+      &::placeholder {
+        color: rgba(255, 255, 255, 0.7);
+      }
+    }
+
+    span {
+      color: #bf616a;
+      align-self: flex-start;
+      margin: 0 0 10px;
+    }
+
+    hr {
+      border: 0;
+      height: 1px;
+      background: rgba(255, 255, 255, 0.1);
+      margin: 10px 0 20px;
+    }
+
+    button {
+      margin: 10px 0 0;
+      height: 44px;
+      background: #3b4252;
+      font-weight: bold;
+      color: #eceff4;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.03, '#3b4252')};
+      }
+    }
+
+    a {
+      color: #eceff4;
+      margin-top: 15px;
+      font-size: 16px;
+      opacity: 0.8;
+
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
+
+  > button {
+    width: 100%;
+    margin: 10px 0 0;
+    height: 44px;
+    background: #d08770;
+    font-weight: bold;
+    color: #eceff4;
+    border: 0;
+    border-radius: 4px;
+    font-size: 16px;
+    transition: background 0.2s;
+
+    &:hover {
+      background: ${darken(0.1, '#d08770')};
+    }
+  }
+`;
